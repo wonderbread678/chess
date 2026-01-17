cfd390015bce3f8322494dab3731b4d526f74363
wonderbread678 <wonderbread678@gmail.com>
2026-01-16 22:49:50 -0700

    added pseudo code for the remaining piece moves

--- shared/src/main/java/chess/PieceMoveCalculator.java
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -94,56 +94,19 @@ public class PieceMoveCalculator {
                 // CM = true;
         // DIAGONAL UP AND TO THE LEFT
         // while(CM == true){
-            // Increment startPosition <-1, 1> if it won't go past the edge of the board
-            // if it will go past the edge of the board:
-                // return CM = false
-            // Otherwise, check if there is a piece at that position
-                // if there is a piece, check the piece color
-                    // If its an opposing piece:
-                        // add the position to the moveList
-                        // return CM = false 
-                    // else
-                        // return CM = false
-            // If there is not a piece, and it is within the board space:
-                // add the new position to moveList
-                // return CM = true
+            // Repeat, but increment startPosition <-1, 1> if it won't go past the edge of the board
         // }
         // CM = true;
         // DIAGONAL DOWN AND TO THE LEFT
         // while(CM == true){
-            // Decrement startPosition <-1, -1> if it won't go past the edge of the board
-            // if it will go past the edge of the board:
-                // return CM = false
-            // Otherwise, check if there is a piece at that position
-                // if there is a piece, check the piece color
-                    // If its an opposing piece:
-                        // add the position to the moveList
-                        // return CM = false 
-                    // else
-                        // return CM = false
-            // If there is not a piece, and it is within the board space:
-                // add the new position to moveList
-                // return CM = true
+            // Repeat, but decrement startPosition <-1, -1> if it won't go past the edge of the board
         // }
         // CM = true;
         // DIAGONAL DOWN AND TO THE RIGHT
         // while(CM == true){
-            // Decrement startPosition <1, -1> if it won't go past the edge of the board
-            // if it will go past the edge of the board:
-                // return CM = false
-            // Otherwise, check if there is a piece at that position
-                // if there is a piece, check the piece color
-                    // If its an opposing piece:
-                        // add the position to the moveList
-                        // return CM = false 
-                    // else
-                        // return CM = false
-            // If there is not a piece, and it is within the board space:
-                // add the new position to moveList
-                // return CM = true
+            // Repeat, but decrement startPosition <1, -1> if it won't go past the edge of the board
         // }
         // return moveList
-        
         
         throw new RuntimeException("Not implemented");
     }
@@ -151,29 +114,79 @@ public class PieceMoveCalculator {
 
     public ChessPosition moveLeft(ChessPosition startPosition){
         // Pseudo-code:
+        // CM = true;
+        // while(CM == true){
+            // Decrement startPosition X-value by 1 if it won't go past the edge of the board
+            // if it will go past the edge of the board:
+                // return CM = false
+            // Otherwise, check if there is a piece at that position
+                // if there is a piece, check the piece color
+                    // If its an opposing piece:
+                        // add the position to the moveList
+                        // return CM = false 
+                    // else
+                        // return CM = false
+            // If there is not a piece, and it is within the board space:
+                // add the new position to moveList
+                // return CM = true
+        // return moveList
+        // }
 
         throw new RuntimeException("Not implemented");
     }
     public ChessPosition moveRight(ChessPosition startPosition){
         // Pseudo-code:
+        // CM = true;
+        // while(CM == true){
+            // Increment startPosition X-value by 1 if it won't go past the edge of the board
+            // if it will go past the edge of the board:
+                // return CM = false
+            // Otherwise, check if there is a piece at that position
+                // if there is a piece, check the piece color
+                    // If its an opposing piece:
+                        // add the position to the moveList
+                        // return CM = false 
+                    // else
+                        // return CM = false
+            // If there is not a piece, and it is within the board space:
+                // add the new position to moveList
+                // return CM = true
+        // return moveList
+        // }
 
         throw new RuntimeException("Not implemented");
     }
 
     public ChessPosition moveWhitePawn(ChessPosition startPosition, ChessPosition previousMove){
         // Pseudo-code:
+        // Check if the pawn can move 1 space or two
+            // Based on that, increment start-position y-value by 1 or 2
+            // check if there is a piece blocking the pawn and what color that piece is
+            // Otherwise, add the new position to moveList
+        // return moveList
 
         throw new RuntimeException("Not implemented");
     }
 
     public ChessPosition moveBlackPawn(ChessPosition startPosition, ChessPosition previousMove){
         // Pseudo-code:
+        // Check if the pawn can move 1 space or two
+            // Based on that, decrement start-position y-value by 1 or 2
+            // check if there is a piece blocking the pawn and what color that piece is
+            // Otherwise, add the new position to moveList
+        // return moveList
 
         throw new RuntimeException("Not implemented");
     }
 
     public ChessPosition moveKnight(ChessPosition startPosition){
         // Pseudo-code:
+        // Have two lists: one that contains all of the possible moves a knight can move, and one that is empty that will hold possibleMoves
+        // Define all of the possibilites that a knight is able to move
+        // Based on the start position, check each one and see if it would go outside of the board. If it is a possiblity, add it to the possibleMoves list.
+        // Then check if there is a piece at the possible positions and what color they are, if applicable
+        // If there isn't a same-color piece, add the new position to moveList
+        // return moveList
 
         throw new RuntimeException("Not implemented");
     }
