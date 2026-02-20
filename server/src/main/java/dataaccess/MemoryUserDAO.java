4019471a455d6155f1bfa12276d5c2721277cdbc
wonderbread678 <wonderbread678@gmail.com>
2026-02-20 00:27:48 -0700

    I believe i've implemented the necessary items for MemoryUserDAO based on the spec. Not toally sure if they work though

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,23 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private final HashMap<String, UserData> allUsers = new HashMap<>();
+
+    public UserData createUser(String username, String password, String email) throws DataAccessException {
+        UserData user = new UserData(username, password, email);
+        allUsers.put(username, user);
+        return user;
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        return allUsers.get(username);
+    }
+
+    public void deleteAllUsers() throws DataAccessException {
+        allUsers.clear();
+    }
 }
