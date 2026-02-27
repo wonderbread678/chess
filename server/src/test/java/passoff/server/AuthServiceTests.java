7b2f3ef4f6db0be637a4102d6f2020591fabfe73
wonderbread678 <wonderbread678@gmail.com>
2026-02-26 23:05:15 -0700

    Finished creating tests for AuthService (all passed)

--- server/src/test/java/passoff/server/AuthServiceTests.java
+++ server/src/test/java/passoff/server/AuthServiceTests.java
@@ -2,11 +2,11 @@ package passoff.server;
 import dataaccess.DataAccessException;
 import dataaccess.*;
 import dataaccess.Memory.MemoryAuthDAO;
+import dataaccess.Memory.MemoryUserDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.ResponseException;
 import service.AuthService;
-import service.GameService;
 import service.UserService;
 import model.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +15,7 @@ public class AuthServiceTests {
 
     static final MemoryAuthDAO authDAO = new MemoryAuthDAO();
     static final AuthService service = new AuthService(authDAO);
+    static final UserService userService = new UserService(authDAO, new MemoryUserDAO());
 
     @BeforeEach
     public void setup() throws ResponseException{
@@ -23,16 +24,31 @@ public class AuthServiceTests {
 
     @Test
     public void testIsAuth() throws ResponseException{
+        AuthData testUser = userService.createUser("test", "test", "test");
 
+        assertTrue(service.isAuth(testUser.authToken()));
     }
 
     @Test
     public void testIsAuthInvalid() throws ResponseException{
-
+        assertFalse(service.isAuth("fakeAuth"));
     }
 
     @Test
-    public void testDeleteAllGames() throws ResponseException{
+    public void testDeleteAllAuth() throws ResponseException{
+        try{
+            authDAO.createAuth(new AuthData("auth1", "user1"));
+            authDAO.createAuth(new AuthData("auth2", "user2"));
+            authDAO.createAuth(new AuthData("auth3", "user3"));
 
+            service.deleteAllAuth();
+
+            assertNull(authDAO.getAuth("auth1"));
+            assertNull(authDAO.getAuth("auth2"));
+            assertNull(authDAO.getAuth("auth3"));
+        }
+        catch(DataAccessException ex){
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
 }
